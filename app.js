8942e333708145ab45aab4368a1432fd5083f0b7
Ogulcan <ogulcanalim@outlook.com>
2023-01-10 03:46:14 +0300

    js animation

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const splash = document.querySelector('.splash')
 
 document.addEventListener('DOMContentLoaded', (e) => {setTimeout(()=>{
     splash.classList.add('display-none');
-},2000);
+},3000);
 })
